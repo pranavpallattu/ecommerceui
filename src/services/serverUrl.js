56f1596d0257dc2b03a88fad1f8a1e95463b41f3
pranavpallattu <pranavpspallattu@gmail.com>
2026-03-29 14:40:49 +0530

    fix: serverUrl

--- src/services/serverUrl.js
+++ src/services/serverUrl.js
@@ -1,3 +1,5 @@
 // export const serverUrl="https://ecommerce-8tjk.onrender.com"
-export const serverUrl = import.meta.env.VITE_SERVER_URL;
+// export const serverUrl = import.meta.env.VITE_SERVER_URL;
 // export const serverUrl= "http://localhost:7777"
+
+export const serverUrl = import.meta.env.VITE_SERVER_URL;
