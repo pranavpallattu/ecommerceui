f81195f1bea8a1e4ea45c1f6980bc03d55059300
pranavpallattu <pranavpspallattu@gmail.com>
2026-04-13 21:31:20 +0530

    fix: google success

--- src/pages/GoogleSuccess.jsx
+++ src/pages/GoogleSuccess.jsx
@@ -2,19 +2,27 @@ import { useEffect } from "react";
 import useAuthStore from "../utils/stores/userAuthStore";
 
 export default function GoogleSuccess() {
-const { user, loading } = useAuthStore();
+  const { user, loading, checkAuth } = useAuthStore();
 
-useEffect(() => {
-  if (loading) return;
+  useEffect(() => {
+    const init = async () => {
+      await checkAuth(); // ✅ THIS WAS MISSING
+    };
 
-  if (user?.isAdmin) {
-    window.location.replace("/admin/dashboard");
-  } else if (user) {
-    window.location.replace("/");
-  } else {
-    window.location.replace("/auth");
-  }
-}, [user, loading]);
+    init();
+  }, []);
+
+  useEffect(() => {
+    if (loading) return;
+
+    if (user?.isAdmin) {
+      window.location.replace("/admin/dashboard");
+    } else if (user) {
+      window.location.replace("/");
+    } else {
+      window.location.replace("/auth");
+    }
+  }, [user, loading]);
 
   return <h1>Logging in...</h1>;
 }
